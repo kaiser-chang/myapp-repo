1b233d2cdf2b39d7731e8176cbafb69775fc082c
EC2 Default User <ec2-user@ip-10-0-10-71.ap-northeast-1.compute.internal>
2022-05-23 04:30:09 +0000

    Change Layout.js

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -38,7 +38,7 @@ export default function Layout() {
                 <HomeIcon />
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                ExamApp
+                Cloud자격시험연습
               </Typography>
             </Toolbar>
           </AppBar>
